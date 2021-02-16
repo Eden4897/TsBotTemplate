417b2ea14f0c15b354232e24a6dd2db3c8b8a47f
Eden <53995141+Eden4897@users.noreply.github.com>
2021-02-16 18:55:19 +0800

    Update template-cleanup.bat

--- template-cleanup.bat
+++ template-cleanup.bat
@@ -1,9 +1,8 @@
 @echo off
-call echo template-cleanup.bat > .\.git\info\exclude
-call echo readme.md >> .\.git\info\exclude
 call echo !package-lock.json >> .\.gitignore
 call echo !package.json >> .\.gitignore
 call git add .
+call git rm --cache template-cleanup.bat readme.md
 call git commit -am "Template Cleanup"
 call git push
 del readme.md
